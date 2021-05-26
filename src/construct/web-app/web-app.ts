dedffc8a263b3bd7f5eaf45f3d92a9616110ba08
badatt <dev.badatt@gmail.com>
2021-05-26 09:29:53 +0530

    sonar addition

--- src/construct/web-app/web-app.ts
+++ src/construct/web-app/web-app.ts
@@ -1,34 +1,34 @@
-import { CfnOutput, Construct } from "@aws-cdk/core";
-import { IHostedZone } from "@aws-cdk/aws-route53";
-import { ICertificate } from "@aws-cdk/aws-certificatemanager";
-import { BlockPublicAccess, Bucket, ObjectOwnership } from "@aws-cdk/aws-s3";
+import { CfnOutput, Construct } from '@aws-cdk/core';
+import { IHostedZone } from '@aws-cdk/aws-route53';
+import { ICertificate } from '@aws-cdk/aws-certificatemanager';
+import { BlockPublicAccess, Bucket, ObjectOwnership } from '@aws-cdk/aws-s3';
 
 export interface WebAppProps {
-	readonly rootDomain: string;
+  readonly rootDomain: string;
 
-	readonly subDomain: string;
+  readonly subDomain: string;
 
-	readonly hostedZone: IHostedZone;
+  readonly hostedZone: IHostedZone;
 
-	readonly certificate: ICertificate;
+  readonly certificate: ICertificate;
 }
 
 export class WebApp extends Construct {
-	public bucket: Bucket;
-	constructor(scope: Construct, id: string, props: WebAppProps) {
-		super(scope, id);
+  public bucket: Bucket;
+  constructor(scope: Construct, id: string, props: WebAppProps) {
+    super(scope, id);
 
-		const hostedZoneName = props.hostedZone.zoneName;
-		const siteUrl = props.subDomain ? props.subDomain + "." + hostedZoneName : hostedZoneName;
+    const hostedZoneName = props.hostedZone.zoneName;
+    const siteUrl = props.subDomain ? `${props.subDomain}.${hostedZoneName}` : hostedZoneName;
 
-		this.bucket = new Bucket(scope, `${id}DeploymentBucket`, {
-			blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-			objectOwnership: ObjectOwnership.BUCKET_OWNER_PREFERRED,
-		});
+    this.bucket = new Bucket(scope, `${id}DeploymentBucket`, {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      objectOwnership: ObjectOwnership.BUCKET_OWNER_PREFERRED,
+    });
 
-		new CfnOutput(this, "WebsiteBucket", {
-			value: this.bucket.bucketName,
-			description: `The deployment bucket for ${siteUrl}`,
-		});
-	}
+    new CfnOutput(this, 'WebsiteBucket', {
+      value: this.bucket.bucketName,
+      description: `The deployment bucket for ${siteUrl}`,
+    });
+  }
 }
--- src/stack/base/base-stack.ts
+++ src/stack/base/base-stack.ts
@@ -1,7 +1,7 @@
-import { Construct, Stack, StackProps } from "@aws-cdk/core";
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
 
 export class BaseStack extends Stack {
-	constructor(scope: Construct, id: string, props?: StackProps) {
-		super(scope, id, props);
-	}
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+  }
 }
